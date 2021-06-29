8eb1cce9e4bbbf20f35a5cbf61e055b456be56c2
Akel <akel.hosni@gmail.com>
2021-06-29 16:20:29 +0100

    first commit

--- src/Components/CocktailsList.js
+++ src/Components/CocktailsList.js
@@ -4,14 +4,25 @@ import Cocktails from './Cocktails';
 
 const CocktailsList = () => {
     const [cocktail,setCocktail]=useState([]);
+    const [search,setSearch]=useState("");
+const handleChange=(e)=>{
+    e.preventDefault();
+    setSearch(e.target.value)
+}
 
     useEffect(() => {
-        axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
         .then((res)=>setCocktail(res.data.drinks))
         .catch((err)=>console.log(err))
-    }, [])
+    }, [search])
     return (
         <div>
+            <div>
+                <form>
+                    <input type='text' placeholder='Search...' onChange={handleChange} value={search} />
+                    <button>Search</button>
+                </form>
+            </div>
             {
                 cocktail.map((el)=>
                     <Cocktails key={el.idDrink} el={el}  />
--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import axios from 'axios';
+import React,{useState,useEffect} from 'react'
+import {useParams,Link} from 'react-router-dom'
 
-const Details = ({match,history}) => {
+const Details = () => {
+    const [drink,setDrink]=useState([]);
+    const {id}=useParams();
+    useEffect(() => {
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then((res)=> setDrink(res.data.drinks[0]))
+        .catch((err)=>console.log(err))
+    }, [])
+
     return (
         <div>
-            <h1>Challenge!!!!</h1>
-            <button onClick={history.goBack} >Go Back</button>
+            <div>
+                <img src={drink.strDrinkThumb} alt="..."/>
+                <h3>Name: {drink.strDrink}</h3>
+                    <div>
+                        <h4>Ingredient 1:</h4>
+                        <h4>Ingredient 2:</h4>
+                        <h4>Ingredient 3:</h4>
+                    </div>
+                <div>
+                    <h5> {drink.strIngredient1}</h5>
+                    <h5> {drink.strIngredient2}</h5>
+                    <h5> {drink.strIngredient3}</h5>
+                </div>
+            </div>
+            <Link to="/"><button>Go Back</button></Link>
         </div>
     )
 }
